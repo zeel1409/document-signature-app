4da880002dec344f26a380d136f0e0a159b73bec
Zeel Kadiya <202228567+zeel1409@users.noreply.github.com>
2026-03-07 23:01:05 +0530

    fix: tailwind v3 clean setup

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,4 @@
-//  @type {import('tailwindcss').Config} 
-// export default {
-//   content: [],
-//   theme: {
-//     extend: {},
-//   },
-//   plugins: [],
-// }
+
 
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
